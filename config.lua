72c4a73a16301b2eb430d1866c2cfd844b567d70
root <root@quassel-phh.rezel.enst.fr>
2014-05-13 13:25:45 +0000

    Groumpf, forgot chan/nicks containing -

--- config.lua
+++ config.lua
@@ -36,7 +36,7 @@ function autohide_timeout()
 		repeat
 			local t = i:split("-")
 			local net = tonumber(t[1])
-			local buffer = t[2]
+			local buffer = table.concat(table_slice(t, 2, -1), "-")
 			if buffer == nil then
 				break
 			end
@@ -68,4 +68,26 @@ function string:split(sep)
 	local pattern = string.format("([^%s]+)", sep)
 	self:gsub(pattern, function(c) fields[#fields+1] = c end)
 	return fields
+end
+
+function table_slice (values,i1,i2)
+	local res = {}
+	local n = #values
+	-- default values for range
+	i1 = i1 or 1
+	i2 = i2 or n
+	if i2 < 0 then
+		i2 = n + i2 + 1
+	elseif i2 > n then
+		i2 = n
+	end
+	if i1 < 1 or i1 > n then
+		return {}
+	end
+	local k = 1
+	for i = i1,i2 do
+		res[k] = values[i]
+		k = k + 1
+	end
+	return res
 end
